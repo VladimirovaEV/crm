616ff859c172b2071edec052181e573ec783fdf4
Vladimirova Ekaterina <echapyrkina@mail.ru>
2022-06-03 21:49:50 +0300

    lesson 2

---
+++ js/index.js
@@ -0,0 +1,7 @@
+'use strict'
+const modalTitle = document.querySelector('.modal__title');
+const modalForm = document.querySelector('.modal__form');
+const modalCheck = document.querySelector('.vendor-code__btn');
+const descriptionCheck = document.querySelector('.vendor-code__wrapper');
+
+
